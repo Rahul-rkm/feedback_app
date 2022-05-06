9e9f88dbf9dbe8df7faefbbaa68a58f8b4e6363e
Rahul-rkm <skyphyrkm@gmail.com>
2022-05-06 20:05:35 +0530

    push new

--- src/App.js
+++ src/App.js
@@ -1,39 +1,52 @@
-import {v4 as uuidv4} from 'uuid';
+import { v4 as uuidv4 } from 'uuid';
 import { useState } from "react";
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Header from "./components/Header";
 import FeedbackList from "./components/FeedbackList";
 import FeedbackData from './data/FeedbackData';
 import FeedbackStats from "./components/FeedbackStats";
 import FeedbackForm from "./components/FeedbackForm";
+import AboutPage from './components/pages/AboutPage';
 
 function App() {
     // feedback is an array of feedback objects, feedback object represents a feedback
     const [feeddback, setFeedback] = useState(FeedbackData);
 
-    const deleteFeedback = (id) =>{
-        if(window.confirm('Are you sure to delete?')) {
-            setFeedback(()=> feeddback.filter((item)=> item.id !== id))
+    const deleteFeedback = (id) => {
+        if (window.confirm('Are you sure to delete?')) {
+            setFeedback(() => feeddback.filter((item) => item.id !== id))
         }
     }
 
-    const addFeedback = (newFeedback)=> {
+    const addFeedback = (newFeedback) => {
         newFeedback.id = uuidv4();
 
-        setFeedback([newFeedback,...feeddback]);
+        setFeedback([newFeedback, ...feeddback]);
         console.log(newFeedback);
     }
 
     return (
-    <>
-    <Header style={{border: '5px solid red'}} />
-    <div className="container">
-        <FeedbackForm handleAdd={addFeedback}/>
-        <FeedbackStats feedback={feeddback}/>
-        <FeedbackList feedback={feeddback} deletHandle={deleteFeedback}/>
-    </div>
-    </>
+        <>
+            <Header style={{ border: '5px solid red' }} />
+            <div className="container">
+                <Router>
+                    <Routes>
+                        <Route exact path='/' element={
+                            <>
+                                <FeedbackForm handleAdd={addFeedback} />
+                                <FeedbackStats feedback={feeddback} />
+                                <FeedbackList feedback={feeddback} deletHandle={deleteFeedback} />
+                            </>
+                        }>
+                        </Route>
+                        <Route path='/about' element={<AboutPage />} />
+                    </Routes>
+
+                </Router>
+            </div>
+        </>
     )
-    
+
 }
 
 export default App;
--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -1,15 +1,36 @@
 import React from 'react';
+import { motion, AnimatePresence } from 'framer-motion';
 import FeedbackItem from './FeedbackItem';
 import PropType from 'prop-types'
 
 
 function FeedbackList( {feedback, deletHandle } ) {
     if(!feedback || feedback.length === 0) return ( <p>No feedbacks yet.</p> );
-  return (
-    <div className='feedback-list'>{feedback.map((item) =>  (
-        <FeedbackItem key={item.id} item={item} handleDelete={deletHandle}/>
-    ))}</div>
-  )
+  
+    return (
+        <div className='feedback-list'>
+          <AnimatePresence>
+          {feedback.map((item) =>  (
+            <motion.div 
+            key={item.id}
+            initial={{opacity: 0}}
+            animate={{opacity: 1}}
+            exit={{opacity: 0}}
+            >
+              <FeedbackItem key={item.id} item={item} handleDelete={deletHandle}/>
+            </motion.div>
+        ))}
+          </AnimatePresence>
+        </div>
+      )
+  
+
+  // Without animations -------------
+  // return (
+  //   <div className='feedback-list'>{feedback.map((item) =>  (
+  //       <FeedbackItem key={item.id} item={item} handleDelete={deletHandle}/>
+  //   ))}</div>
+  // )
 }
 
 FeedbackList.propTypes = {
